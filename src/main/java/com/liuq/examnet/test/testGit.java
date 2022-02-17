47b017daa21e95b5ee3038de649f1da17b32f433
liuq <lq792715074@126.com>
2022-02-17 17:24:23 +0800

    second commit

---
+++ src/main/java/com/liuq/examnet/test/testGit.java
@@ -0,0 +1,11 @@
+package com.liuq.examnet.test;
+
+/**
+ * @author liuq
+ * @create 2022-02-17-
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("git1");
+    }
+}
